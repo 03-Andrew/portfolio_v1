8c5758d78d98cc1e99040f62c1b00b8d434deac5
03-Andrew <andreinadela052@gmail.com>
2026-05-13 14:41:34 +0800

    feat: update experience data structure and add new entries

--- app/data/experience.ts
+++ app/data/experience.ts
@@ -9,19 +9,19 @@ export interface ExperienceData {
 
 export const experienceData: ExperienceData[] = [
     {
-        logo: 'LG',
-        company: "Freelance",
-        role: "Web Developer",
-        date: "2022 - Present",
-        description: "Developed and maintained websites for various clients, utilizing technologies such as React, Node.js, and MongoDB to create responsive and user-friendly web applications.",
-        techStack: ["React", "Node.js", "MongoDB"]
-    },
-    {
         logo: 'TP',
         company: "Turnkey Philippines",
         role: "Backend Developer Intern",
         date: "Dec 2025 - March 2026",
         description: "Built and deployed the backend infrastructure for a client mobile application with a team of interns. Developed RESTful APIs using Fastify, designed scalable database schemas, and hosted both the application and self-managed n8n workflows on DigitalOcean. Implemented CI/CD pipelines for streamlined deployments while gaining hands-on experience in cloud infrastructure, backend architecture, and development workflows.",
         techStack: ["Fastify", "n8n", "DigitalOcean", "TypeScript", "Supabase"]
+    },
+    {
+        logo: 'MP',
+        company: "Mapua Malayan Colleges Mindanao",
+        role: "BS Computer Science Student",
+        date: "2022 - 2026",
+        description:"Consistent Dean's Lister with coursework focused on data structures, algorithms, databases, software engineering, machine learning, and web development.",
+        techStack: [ "Data Structures & Algorithms","Machine Learning","Software Engineering"]
     }
 ]
